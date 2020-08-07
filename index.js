143ba6ccc84316add3805784889c872fe7bbf196
agusrichard <agus.richard21@gmail.com>
2020-08-07 20:35:42 +0700

    Initializing First

--- index.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-const { sum, multiply } = require('./src/func.js');
-
-console.log(sum(21, 14));
-console.log(multiply(3, 5));
--- percabangan.js
+++
@@ -1,8 +0,0 @@
-const appname = 'Practice';
-const appversion = '1.0.0';
-
-if (appname === 'Practice') {
-  console.log(appname);
-} else {
-  console.log('AppName is not match');
-}
